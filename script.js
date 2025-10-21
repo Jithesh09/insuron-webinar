80b2f11cc622fa8f8171b6c310ba7a6bdd931cce
nathangopi <nathangopi@users.noreply.github.com>
2025-10-21 13:57:57 +0530

    Changes FORM POST url to subdomain

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.querySelectorAll('a[href^="#"]').forEach(a=>{
     };
 
     try {
-  const response = await fetch("https://insuron.in/webinar.php", {
+  const response = await fetch("https://webinar.insuron.in/webinar.php", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
